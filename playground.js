04b1189643882578453670055a541597280cba1f
Babajide Ibiayo <93061524+bibiayo@users.noreply.github.com>
2023-05-01 08:28:43 +0100

    added product color options

--- playground.js
+++ playground.js
@@ -1,6 +1,8 @@
 const add2cart = document.querySelector(".add2cart");
-const qty = document.querySelector('.qty');
 const totalPrice = document.querySelector(".total-price")
+const qty = document.querySelector(".qty");
+const product = document.querySelector(".product");
+const productThumbs = document.querySelectorAll('.product-thumb');
 const productPrice = 24.5;
 
 let counter = 0;
@@ -12,6 +14,15 @@ add2cart.addEventListener('click', function(e) {
     qty.textContent = counter;
     total = total + productPrice;
     totalPrice.textContent = total.toFixed(2);
+})
+
+function updateProduct(thumb) {
+    const path = thumb.getAttribute('src')
+    product.setAttribute('src', path)
+}
+
+productThumbs.forEach( thumb => {
+    thumb.addEventListener('click', () => updateProduct(thumb))
 })
 
 
