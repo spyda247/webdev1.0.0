74feac2e601c739269c24413fbca0ab1e24b8aa1
Babajide Ibiayo <93061524+bibiayo@users.noreply.github.com>
2023-05-01 12:31:32 +0100

    modified Playground

--- playground.js
+++ playground.js
@@ -1,28 +1,27 @@
 const add2cart = document.querySelector(".add2cart");
-const totalPrice = document.querySelector(".total-price")
+const totalPrice = document.querySelector(".total-price");
 const qty = document.querySelector(".qty");
 const product = document.querySelector(".product");
-const productThumbs = document.querySelectorAll('.product-thumb');
+const productThumbs = document.querySelectorAll(".product-thumb");
 const productPrice = 24.5;
 
 let counter = 0;
 let total = 0;
 
-add2cart.addEventListener('click', function(e) {
-    e.preventDefault();
-    counter++;
-    qty.textContent = counter;
-    total = total + productPrice;
-    totalPrice.textContent = total.toFixed(2);
-})
+add2cart.addEventListener("click", function (e) {
+  e.preventDefault();
+  counter++;
+  qty.textContent = counter;
+  total = total + productPrice;
+  totalPrice.textContent = total.toFixed(2);
+});
 
 function updateProduct(thumb) {
-    const path = thumb.getAttribute('src')
-    product.setAttribute('src', path)
+  const path = thumb.getAttribute("src");
+  product.setAttribute("src", path);
+  console.log(thumb);
 }
 
-productThumbs.forEach( thumb => {
-    thumb.addEventListener('click', () => updateProduct(thumb))
-})
-
-
+productThumbs.forEach((thumb) => {
+  thumb.addEventListener("click", () => updateProduct(thumb));
+});
